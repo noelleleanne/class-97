bb047470fb3f043f32349a06089b259309b7b626
noelleleanne <72886830+noelleleanne@users.noreply.github.com>
2021-05-14 08:31:16 -0600

    Add files via upload

---
+++ pocketMoney.py
@@ -0,0 +1,7 @@
+pocketMoney = int(input("enter the pocket money you get every month: "))
+if(pocketMoney > 500):
+    print("you are a rich kid")
+elif(pocketMoney > 100):
+    print("you have a good life")
+else:
+    print("i know how you feel")
